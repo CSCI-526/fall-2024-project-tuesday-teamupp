320ded2f8e5bdfe3657ac6dc5a101692c93fad43
Augusto Rivas <rivasaugusto10@gmail.com>
2024-10-19 12:38:21 -0700

    Adding static text for portal and changed the alignment on the pop-up texts

--- GalaxyMania/Assets/PlayerController.cs
+++ GalaxyMania/Assets/PlayerController.cs
@@ -21,7 +21,6 @@ public class PlayerController : MonoBehaviour
     public Transform levelParent;
     private bool isGameOver = false; // Track if the game is over
     private bool isBeyondThreshold = false;
-    private bool hasMoved = false; // Flag to track if the player has moved for the first time
     public float borderThresholdDistance = 300f;
     private Collider2D borderCollider;  
     private bool isCheckingDistance = false;
@@ -72,18 +71,6 @@ public class PlayerController : MonoBehaviour
     {
         moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
-
-        if (!hasMoved && moveInput != 0 && SceneManager.GetActiveScene().name == "Level 1")
-        {
-            hasMoved = true; // Set the flag so this only triggers once
-
-            // Find the PowerUpPopUp script and display the info text
-            PowerUpPopUp popUp = FindObjectOfType<PowerUpPopUp>();
-            if (popUp != null)
-            {
-                popUp.ShowInfoText("Welcome to Level 1! \n Use Arrow Keys to move around and explore.");
-            }
-        }
     }
 
     void Jump()
