adff48f980a55cc70a77939a304c524703b3fbf1
Augusto Rivas <rivasaugusto10@gmail.com>
2024-10-22 01:32:24 -0700

    Cleanups

--- GalaxyMania/Assets/DistTracker.cs
+++ GalaxyMania/Assets/DistTracker.cs
@@ -1,8 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
